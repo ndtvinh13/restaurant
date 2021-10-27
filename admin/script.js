98de96ee7a978ff5fc221403c86c31c52792f016
ndtvinh13 <ndtvinh14@gmail.com>
2021-10-26 23:44:39 -0700

    Updated with Admin and css

---
+++ admin/script.js
@@ -0,0 +1,27 @@
+// console.log("hello world");
+
+// const nav_link = document.querySelectorAll(".nav-link");
+// const nav_item = document.querySelectorAll(".nav-item");
+// //const active_tab = document.querySelector(".active-tab");
+
+// for (var i = 0; i < nav_link.length; i++) {
+//   console.log(nav_link[i]);
+//   nav_link[i].addEventListener("click", function () {
+//     console.log(nav_item[i]);
+//     nav_item[i + 1].classList.add("active-tab");
+//   });
+// }
+
+// var i = 0;
+
+// nav_link[0].addEventListener("click", function () {
+//   nav_item[0].classList.add("active-tab");
+//   nav_item[1].classList.remove("active-tab");
+// });
+
+// nav_link[1].addEventListener("click", function () {
+//   nav_item[0].classList.remove("active-tab");
+//   nav_item[1].classList.add("active-tab");
+// });
+
+console.log(nav_item);
